89cfd506f556c31c1d3d8cd132c56808cfa87879
khvorost <khvorosttt@yandex.by>
2021-12-07 20:32:27 +0300

    Create Vector.cs

---
+++ Lab_4/Vector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexCalculator
+{
+    class Vector
+    {
+        Hex[] numbers;
+        public Vector()
+        {
+            numbers = new Hex[7];
+        }
+        public Vector(int n)
+        {
+            numbers = new Hex[n];
+        }
+
+        public Hex this[int index]
+        {
+            get
+            {
+                return numbers[index];
+            }
+            set
+            {
+                numbers[index] = value;
+            }
+        }
+
+        public int Length()
+        {
+            return numbers.Length;
+        }
+    }
+}
